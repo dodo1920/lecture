c5925871ab43bc7eb61ce2e9143bb100fd200720
do <ssonagi032@gmail.com>
2021-02-22 16:19:03 +0900

    BoardVO에서 Content 추가 및 수정

--- src/main/java/com/dodo1920/domain/BoardVO.java
+++ src/main/java/com/dodo1920/domain/BoardVO.java
@@ -6,55 +6,76 @@ public class BoardVO {
 	private int no;
 	private String title;
 	private String writer;
+	private String content;
 	private Date regdate;
 	private int viewcnt;
 	
 	public BoardVO() {
 		super();
 	}
-	public BoardVO(int no, String title, String writer, Date regdate, int viewcnt) {
+
+	public BoardVO(int no, String title, String writer, String content, Date regdate, int viewcnt) {
 		super();
 		this.no = no;
 		this.title = title;
 		this.writer = writer;
+		this.content = content;
 		this.regdate = regdate;
 		this.viewcnt = viewcnt;
 	}
-	
+
 	public int getNo() {
 		return no;
 	}
+
 	public void setNo(int no) {
 		this.no = no;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getWriter() {
 		return writer;
 	}
+
 	public void setWriter(String writer) {
 		this.writer = writer;
 	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
 	public Date getRegdate() {
 		return regdate;
 	}
+
 	public void setRegdate(Date regdate) {
 		this.regdate = regdate;
 	}
+
 	public int getViewcnt() {
 		return viewcnt;
 	}
+
 	public void setViewcnt(int viewcnt) {
 		this.viewcnt = viewcnt;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "BoardVO [no=" + no + ", title=" + title + ", writer=" + writer + ", viewcnt=" + viewcnt + "]";
+		return "BoardVO [no=" + no + ", title=" + title + ", writer=" + writer + ", content=" + content + ", regdate="
+				+ regdate + ", viewcnt=" + viewcnt + "]";
 	}
 	
 }
