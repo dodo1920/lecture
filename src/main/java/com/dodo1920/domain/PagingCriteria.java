c4b0d31a176780758144a4363738ff24edb8437d
do <ssonagi032@gmail.com>
2021-02-24 14:37:21 +0900

    페이징 처리에 피요한 기본 제약조건(pagingCriteria)클래스 생성

---
+++ src/main/java/com/dodo1920/domain/PagingCriteria.java
@@ -0,0 +1,42 @@
+package com.dodo1920.domain;
+
+public class PagingCriteria {
+	private int page; // 시작 페이지
+	private int perPageNum; // 1페이지당 출력 글 갯수
+	
+	public PagingCriteria() {
+		this.page = 1;
+		this.perPageNum = 10;
+	}
+	
+	public void setPage(int page) {
+		if(page <= 0) {
+			this.page = 1;
+			return;
+		}
+		
+		this.page = page;
+	}
+	
+	public void setPerPageNum(int perPageNum) {
+		if(perPageNum <= 0 || perPageNum > 81) {
+			this.perPageNum = 10;
+			return;
+		}
+		this.perPageNum = perPageNum;
+	}
+	
+	public int getPageStart() {
+		return (this.page -1) * 10;
+	}
+	
+	public int getPerPageNum() {
+		return this.perPageNum;
+	}
+
+	@Override
+	public String toString() {
+		return "PagingCriteria [page=" + page + ", perPageNum=" + perPageNum + "]";
+	}
+	
+}
