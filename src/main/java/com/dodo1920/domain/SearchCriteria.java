638ae1ff64f894f37797b8a52df683016006b77d
do <ssonagi032@gmail.com>
2021-02-25 10:49:48 +0900

    com.dodo1920.domain.SearchCriteria 생성 (게시글 검색기능을 위한 DTO)

---
+++ src/main/java/com/dodo1920/domain/SearchCriteria.java
@@ -0,0 +1,25 @@
+package com.dodo1920.domain;
+
+public class SearchCriteria {
+	private String searchType; // 검색 종류
+	private String searchWord; // 검색어
+	
+	public String getSearchType() {
+		return searchType;
+	}
+	public void setSearchType(String searchType) {
+		this.searchType = searchType;
+	}
+	public String getSearchWord() {
+		return searchWord;
+	}
+	public void setSearchWord(String searchWord) {
+		this.searchWord = searchWord;
+	}
+	
+	@Override
+	public String toString() {
+		return "SearchCriteria [searchType=" + searchType + ", searchWord=" + searchWord + "]";
+	}
+	
+}
